79f359b83da70f91c0945d98f283012cf951f96c
Scott Falbo <scottfalboart@gmail.com>
2025-03-09 03:02:14 -0700

    established wiring through frontend

---
+++ NecronomiconVitae.CorpusMaleficarum/Pages/Grimoire.razor.cs
@@ -0,0 +1,26 @@
+﻿// ------------------------------------
+// Necronomicon Vitae
+// ------------------------------------
+
+using Microsoft.AspNetCore.Components;
+using NecronomiconVitae.CorpusMaleficarum.Models;
+
+namespace NecronomiconVitae.CorpusMaleficarum.Pages;
+
+public partial class Grimoire : ComponentBase
+{
+    private IList<Image> Images = [];
+
+    [Inject]
+    private IProcessor _processor { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var images = await _processor.GetImages();
+
+        if (images != null)
+        {
+            Images = images.ToList();
+        }
+    }
+}
